66c2073d5b70cf499341777d71c7a711b5c38912
Keith Prifte <kprift@gmail.com>
2016-02-23 13:01:05 -0500

    adding effect to only show 2 card values at a time

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ var pairs = ["a", "b", "c", "d", "e", "f", "a", "b", "c", "d", "e", "f"]
 var userGuesses = []
 var userPoints = []
 var matches = []
+var clicks = []
+var cardOne = []
+var cardTwo = []
 
 control = document.body.querySelector(".control")
-cards = control.querySelectorAll(".column1")
+var cards = control.querySelectorAll(".column1")
 section = document.body.querySelector(".section")
 
 score = section.querySelector("p")
@@ -17,16 +20,32 @@ function printCards() {
   for (i = 0; i < cards.length; i++) {
     cards[i].innerHTML = pairs[i];
     cards[i].addEventListener('click', function() {
+
       userGuesses.push({innerHTML: this.innerHTML, click: userGuesses.length});
-    })
+
+      clicks++
+      if (clicks === 1) {
+        cardOne.push(this)
+      } else if (clicks === 2) {
+        cardTwo.push(this)
+      } else if (clicks > 2) {
+        cardOne = cardOne.splice()
+        cardTwo = cardTwo.splice()
+        clicks = 0
+      } else null
+
+      this.style.color = "white"
+        })
   }
 } printCards()
+
 
   document.body.addEventListener("mouseover", function() {
     score.innerHTML = "Score: " + matches.length
   });
 
-  document.body.addEventListener("click", findMatches);
+
+document.body.addEventListener("click", findMatches);
 
 //this function continuosly pushes the matched user guesses to the matches array, creates many matches but they are actually duplicates.  Should fix, but it doesn't affect disappearCards in a negative way
   function findMatches() {
@@ -36,6 +55,8 @@ function printCards() {
             matches.push(userGuesses[j].innerHTML)
         } else null
     }
+
+document.body.addEventListener("mouseover", disappearCards);
 
 function disappearCards() {
     for (z = 0; z < matches.length; z++)
